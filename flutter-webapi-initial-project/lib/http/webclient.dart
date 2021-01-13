c8aeafddb52c3478bb8938f21b5743f8811d02ce
Gabriela Santos <gabriela.pereira.santos@usp.br>
2021-01-13 14:24:00 -0300

    web client
    web client

--- flutter-webapi-initial-project/lib/http/webclient.dart
+++ flutter-webapi-initial-project/lib/http/webclient.dart
@@ -1,6 +1,31 @@
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({RequestData data}) async {
+    print('Request');
+    print('url: $data.baseUrl');
+    print('body: $data.body');
+    print('header: $data.headers');
+
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({ResponseData data}) async {
+    print('Response');
+    print('body: $data.body');
+    print('header: $data.headers');
+    print('status code: $data.statusCode');
+    print(data);
+    return data;
+  }
+}
 
 void findAll() async {
-  final Response response = await get('http://192.168.0.7:8080/transactions');
-  print(response.body);
+  final Client client =
+      HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
+  final Response response =
+      await client.get('http://192.168.0.7:8080/transactions');
 }
