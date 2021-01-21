39ae764c70f063e37490f548cb85d88b57d23d97
Gabriela Santos <gabriela.pereira.santos@usp.br>
2021-01-21 12:38:42 -0300

    -
    -

--- flutter-webapi-initial-project/lib/http/webclient.dart
+++ flutter-webapi-initial-project/lib/http/webclient.dart
@@ -3,7 +3,7 @@ import 'package:http_interceptor/http_interceptor.dart';
 
 import 'interceptors/interceptor.dart';
 
-const String baseUrl = 'http://192.168.0.7:8080/transactions';
+const String baseUrl = '';
 
 final Client client = HttpClientWithInterceptor.build(
   interceptors: [LoggingInterceptor()],
