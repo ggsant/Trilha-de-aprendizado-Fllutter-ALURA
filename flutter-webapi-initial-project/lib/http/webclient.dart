6c15ca85954d6b63bf1e81c56c5f72e6573ec489
Gabriela Santos <gabriela.pereira.santos@usp.br>
2021-02-16 19:17:54 -0300

    atualização
    atualização

--- flutter-webapi-initial-project/lib/http/webclient.dart
+++ flutter-webapi-initial-project/lib/http/webclient.dart
@@ -3,7 +3,6 @@ import 'package:http_interceptor/http_interceptor.dart';
 
 import 'interceptors/interceptor.dart';
 
-//TODO: Apagar o ip
 const String baseUrl = 'http://192.168.0.7:8080/transactions';
 
 final Client client = HttpClientWithInterceptor.build(
